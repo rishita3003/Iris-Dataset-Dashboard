fc361bdda93b0ecf3c7db8fad80ad61bdd07c2c4
Rishita <rishita@gmail.com>
2025-01-25 10:31:09 +0530

    Data processing added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Iris-Dataset-Dashboard
+
+## Instructions to run
+
+Add 
+options(shiny.port = 3838)
+Sys.setenv(RSTUDIO_PANDOC = "C:/Users/Rishita/AppData/Local/Pandoc")
+
+
+## Run
+rmarkdown::run("iris_dashboard.Rmd", shiny_args = list(launch.browser = TRUE))
---
+++ Scripts/processing.R
@@ -0,0 +1,55 @@
+
+
+library(dplyr)
+library(readr)
+library(tidyverse)
+library(qs)
+
+
+data(iris)
+
+#print(head(iris))
+
+iris_processed <- iris %>%
+  mutate(
+    id = row_number(),
+    Species = as.factor(Species),
+    Sepal.Area = Sepal.Length * Sepal.Width,
+    Petal.Area = Petal.Length * Petal.Width
+  )
+
+print(head(iris_processed))
+
+# Calculate summary statistics
+iris_summary <- iris_processed %>%
+  group_by(Species) %>%
+  summarise(
+    across(where(is.numeric), list(
+      mean = ~mean(.x, na.rm = TRUE),
+      sd = ~sd(.x, na.rm = TRUE),
+      min = ~min(.x, na.rm = TRUE),
+      max = ~max(.x, na.rm = TRUE)
+    ))
+  )
+
+print(iris_summary)
+
+# Save as RDS files
+saveRDS(iris_processed, "data/iris/iris_processed.rds")
+saveRDS(iris_summary, "data/iris/iris_summary.rds")
+saveRDS(iris, "data/iris/iris_raw.rds")
+
+qsave(iris_processed, "data/iris/iris_processed.qs")
+qsave(iris_summary, "data/iris/iris_summary.qs")
+qsave(iris, "data/iris/iris_raw.qs")
+
+# Create metadata
+metadata <- data.frame(
+  filename = c("iris_raw.rds", "iris_processed.rds", "iris_summary.rds",
+              "iris_raw.qs", "iris_processed.qs", "iris_summary.qs"),
+  format = c(rep("RDS", 3), rep("QS", 3)),
+  description = rep(c("Original iris dataset",
+                     "Processed dataset with additional features",
+                     "Summary statistics by species"), 2)
+)
+saveRDS(metadata, "data/metadata/meta.rds")
